1c4741e58c3bc43f8838933b2f3f6a9d69635b63
Zabuza Momochi <dev.mizuki94@gmail.com>
2021-05-19 12:40:16 +0200

    Update suggestion.js

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -3,13 +3,13 @@ module.exports = {
     run: async(message, args) => {
       if (!args[0]) return message.channel.send("Indique la suggestion que tu veux faire...")
         var suggestion=message.content.split(" ").splice(1).join(" ");
-        
+        const user=message.author
 
         message.channel.send(new Discord.MessageEmbed()
 .setTitle('__Suggestion__')
 .setColor('RANDOM')
 .setDescription("Il propose :\n"+"**"+suggestion+"**")
-.setThumbnail(message.autor.displayAvatarURL())
+.setThumbnail(user.displayAvatarURL())
 .setFooter('Suggestion de '+message.author.username)
 .setTimestamp())
 },
