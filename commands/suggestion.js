46fac915d71dc031139994ed55b1a765db9a8554
Zabuza Momochi <dev.mizuki94@gmail.com>
2021-05-19 14:51:31 +0200

    Update suggestion.js

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -4,15 +4,9 @@ module.exports = {
       if (!args[0]) return message.channel.send("Indique la suggestion que tu veux faire...")
         var suggestion=message.content.split(" ").splice(1).join(" ");
         const user=message.author
-        const salaude=836242641204543558
+        
+        member.guild.channels.cache.get(config.greeting.sug).send("**__Suggestions de "+ message.author.username+"__**\n"+ suggestion)
 
-        const EMBEDSUG=new Discord.MessageEmbed
-        EMBEDSUG.setTitle('__Suggestion__')
-        EMBEDSUG.setColor('RANDOM')
-        EMBEDSUG.setDescription("Il propose de :\n"+"**"+suggestion+"**")
-        EMBEDSUG.setThumbnail(user.displayAvatarURL())
-        EMBEDSUG.setFooter('Suggestion de '+message.author.username)
-        message.salaude.send(EMBEDSUG)
 
 },
     name : 'sugg'
