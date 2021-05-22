e53b4022244468b0c368b91ee2b19b320da8c29e
Zabuza Momochi <dev.mizuki94@gmail.com>
2021-05-22 17:29:55 +0200

    Create reunion.js

---
+++ commands/reunion.js
@@ -0,0 +1,10 @@
+const Discord = require ('discord.js')
+module.exports = {
+    run: message => {
+        message.channel.send(new Discord.MessageEmbed()
+        .setTitle("Récap Réunion du 22/05/2021")
+        .setColor("RANDOM")
+.setDescription("**→ Nouvel Effectif**\n\n**Avertissement :\nMystraal**\n\n**→ Maintenant les membres ne peuvent plus postuler pour devenir modérateur. Ils devront postuler pour devenir helpeur et passer au moins deux mois en tant que tel avant de pouvoir devenir modérateur**\n\n**→ Enlever les perms de kick aux modos**\n\n**→ Faire un bot anti spam**\n\n**→ Minimum 1000messages pour devenir helpeur**\n\n**Suggestions :**\n\n→ Grand tournoi avec une quinzaine de graphistes.\nEt il y aura 7 juges : 4 juges et 3 staff.\nLe 1er gagne 2 nitros, un compte adn ou crunchyroll et un rôle graphiste en diamant.\nLe 2e gagne 1 nitro et un rôle graphiste en or.\n\n→ Trailer (coucours avec  1 monteur, 1 dessinateur et un graphiste)\n\n→ + de comptes (TikTok, insta, etc....)pour le serveur")
+.setTimestamp())
+    }
+}
