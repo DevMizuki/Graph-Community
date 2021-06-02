0f125b06cd90ce26ad95385058a863e5b4b8755c
Zabuza Momochi <dev.mizuki94@gmail.com>
2021-06-02 10:38:34 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -7,7 +7,7 @@ module.exports = {
         const member = message.mentions.members.first()
         if (!member) return message.channel.send('Veuillez mentionner le membre à bannir.')
         if (member.id === message.guild.ownerID) return message.channel.send('Vous ne pouvez pas bannir le propriétaire du serveur.')
-       
+        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas Bannir ce membre.')
         if (!member.bannable) return message.channel.send('Le bot ne peut pas bannir ce membre.')
         const reason = args.slice(1).join(' ') || 'Aucune raison fournie'
         await member.ban({reason})
