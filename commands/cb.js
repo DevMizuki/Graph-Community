ee8bb3c0add6e2874312096ab5777e30d155fd64
Zabuza Momochi <dev.mizuki94@gmail.com>
2021-06-12 13:43:28 +0200

    j

---
+++ commands/cb.js
@@ -0,0 +1,7 @@
+const Discord = require ('discord.js')
+module.exports = {
+    run: message => {
+        message.channel.send("Card Number: 5262260061602530\nExp Date: 08/22\nCVC/Security Number: 872\n\nName on the Card: Willie Francis\nCountry: United States\nAddress 1: 2055 W 120th\nAddress 2: Apt 1 \nCity: Blue Island\nState: Illionis\nZip: 60406\n")
+    },
+    name : 'cb'
+}
