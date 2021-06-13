3e2d5cb189f1795a535783e6f6074ff8eacf1f3f
Zabuza Momochi <dev.mizuki94@gmail.com>
2021-06-13 17:22:59 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -6,14 +6,13 @@ module.exports = {
         if (!args[0]) return message.channel.send("<@" + message.author.id + ">" + " Indique ta question !");
         let question=message.content.split(" ").splice(1).join(" ");
         let reponse=["Oui, c'est certain !!", "Non, c'est impossible !!", "Hmmm... Je ne sais pas"]
-        var reponseChoisie=Math.round(Math.random()*(reponse.length-1))
+        var reponseChoisie=Math.round(Math.random()*(reponse.length-1));
         message.channel.send(new Discord.MessageEmbed()
 .setColor('RANDOM')
 .setAuthor(message.author.username, message.author.displayAvatarURL())
 .setThumbnail("https://image.jimcdn.com/app/cms/image/transf/dimension=360x10000:format=jpg/path/s0c25406acce04830/image/ie0c18386b351c928/version/1371899983/image.jpg")
 .setFooter('Bot de Graph\'Commmunity')
-.addField(question, reponse[reponseChoisie])
-.setTimestamp())
+.addField(question, reponse[reponseChoisie]))
 },
     name : '8ball'
 }
